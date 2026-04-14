012aa4e0917f200dfecb3d72d9fa48bf395c0c62
Wiktor Mazur <255653@edu.p.lodz.pl>
2026-04-14 23:17:54 +0200

    9.1 Zmiana atrybutow klas pod kątem widoczności

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -7,18 +7,18 @@ public class DefaultCountingOutRhymer {
     private static final int LAST_INDEX = CAPACITY - 1;
     private final int[] numbers = new int[CAPACITY];
 
-    public int total = EMPTY_INDEX;
+    private int total = EMPTY_INDEX;
 
-    public void countIn(int in) {
+    protected void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
-    public boolean callCheck() {
+    protected boolean callCheck() {
         return total == EMPTY_INDEX;
     }
 
-    public boolean isFull() {
+    protected boolean isFull() {
         return total == LAST_INDEX;
     }
 
